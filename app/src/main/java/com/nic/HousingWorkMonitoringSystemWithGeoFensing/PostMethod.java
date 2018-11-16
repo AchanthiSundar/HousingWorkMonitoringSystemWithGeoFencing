54b4829bf22e82d83694c6c9548dbb87a5a33889
Achanthi Sundar M <m.s.achanthisundar@gmail.com>
2018-11-16 16:51:05 +0530

    Full Project Commit To Github Change In Post Method because of Http Issue Add SimpleSSLSocketFactory for HTTP Issue

--- app/src/main/java/com/nic/HousingWorkMonitoringSystemWithGeoFensing/PostMethod.java
+++ app/src/main/java/com/nic/HousingWorkMonitoringSystemWithGeoFensing/PostMethod.java
@@ -6,14 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 
 import android.content.Context;
 
@@ -22,6 +33,7 @@ public class PostMethod {
 	static String param[];
 	static String paramValue[];
 	String Url;
+	DefaultHttpClient httpClient = null;
 
 	Context context;
 	static String responseText;
@@ -34,7 +46,23 @@ public class PostMethod {
 	public String post() {
 
 		try {
-			HttpClient httpclient = new DefaultHttpClient();
+			SSLSocketFactory sslFactory = new SimpleSSLSocketFactory(null);
+			sslFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+			// Enable HTTP parameters
+			HttpParams params = new BasicHttpParams();
+			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+			HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+			// Register the HTTP and HTTPS Protocols. For HTTPS, register our custom SSL Factory object.
+			SchemeRegistry registry = new SchemeRegistry();
+			registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+			registry.register(new Scheme("https", sslFactory, 443));
+
+			// Create a new connection manager using the newly created registry and then create a new HTTP client using this connection manager
+			ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+			httpClient = new DefaultHttpClient(ccm, params);
+
 			HttpPost httppost = new HttpPost(Url);
 
 			int size = param.length;
@@ -50,7 +78,7 @@ public class PostMethod {
 			//HttpResponse response = httpclient.execute(httppost);
 
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			String responseText = httpclient.execute(httppost, responseHandler);
+			String responseText = httpClient.execute(httppost, responseHandler);
 			// System.out.println("Response is : " + responseText);
 
 			return responseText;
@@ -63,7 +91,23 @@ public class PostMethod {
 	public String post1() {
 
 		try {
-			HttpClient httpclient = new DefaultHttpClient();
+			SSLSocketFactory sslFactory = new SimpleSSLSocketFactory(null);
+			sslFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+			// Enable HTTP parameters
+			HttpParams params = new BasicHttpParams();
+			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+			HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+			// Register the HTTP and HTTPS Protocols. For HTTPS, register our custom SSL Factory object.
+			SchemeRegistry registry = new SchemeRegistry();
+			registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+			registry.register(new Scheme("https", sslFactory, 443));
+
+			// Create a new connection manager using the newly created registry and then create a new HTTP client using this connection manager
+			ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+			httpClient = new DefaultHttpClient(ccm, params);
+
 			HttpPost httppost = new HttpPost(Url);
 
 			int size = param.length;
@@ -77,7 +121,7 @@ public class PostMethod {
 			}
 			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(nameValuePairs, "UTF-8");
 			httppost.setEntity(formEntity);			
-			HttpResponse res = httpclient.execute(httppost);
+			HttpResponse res = httpClient.execute(httppost);
 			
 		    BufferedReader in = null; 
 			StringBuffer sb = null;
